568cf4cb10ea28f2360a9232cc7400998a3ec55e
Dasha Nikanova <dasha01120471@gmail.com>
2023-12-18 01:37:38 +0300

    Add choosing libraries to run

--- config.py
+++ config.py
@@ -28,37 +28,43 @@ bigCsvPath = ""
 #path to tiny csv file
 tinyCsvPath = ""
 
+t_DuckDB = True 
+t_Pandas = True
+t_psycopg2 = True
+t_SQLAlchemy = True
+t_SQLite = True
+
 #server in pgadmin
 server = f"postgresql://{user}:{password}@{host}:{port}/{database}"
 
 queries = [ 
 
-    """SELECT "VendorID", COUNT(*)
-    FROM "BebraDB" GROUP BY 1;""",
+    f"""SELECT "VendorID", COUNT(*)
+    FROM "{tableName}" GROUP BY 1;""",
 
-    """SELECT "passenger_count", AVG("total_amount")
-    FROM "BebraDB" GROUP BY 1;""",
+    f"""SELECT "passenger_count", AVG("total_amount")
+    FROM "{tableName}" GROUP BY 1;""",
 
-    """SELECT "passenger_count", EXTRACT(year FROM "tpep_pickup_datetime"), COUNT(*)
-    FROM "BebraDB" GROUP BY 1, 2;""",
+    f"""SELECT "passenger_count", EXTRACT(year FROM "tpep_pickup_datetime"), COUNT(*)
+    FROM "{tableName}" GROUP BY 1, 2;""",
 
-    """SELECT "passenger_count", EXTRACT(year FROM "tpep_pickup_datetime"), ROUND("trip_distance"), COUNT(*)
-    FROM "BebraDB" GROUP BY 1, 2, 3 ORDER BY 2, 4 DESC;""",
+    f"""SELECT "passenger_count", EXTRACT(year FROM "tpep_pickup_datetime"), ROUND("trip_distance"), COUNT(*)
+    FROM "{tableName}" GROUP BY 1, 2, 3 ORDER BY 2, 4 DESC;""",
 
 ]
 
 sqlQueries = [
-    """SELECT "VendorID", COUNT(*)
-    FROM "BebraDB" GROUP BY 1;""",
+    f"""SELECT "VendorID", COUNT(*)
+    FROM "{tableName}" GROUP BY 1;""",
 
-    """SELECT "passenger_count", AVG("total_amount")
-    FROM "BebraDB" GROUP BY 1;""",
+    f"""SELECT "passenger_count", AVG("total_amount")
+    FROM "{tableName}" GROUP BY 1;""",
 
-    """SELECT "passenger_count", STRFTIME('%Y', "tpep_pickup_datetime"), COUNT(*)
-    FROM "BebraDB" GROUP BY 1, 2;""",
+    f"""SELECT "passenger_count", STRFTIME('%Y', "tpep_pickup_datetime"), COUNT(*)
+    FROM "{tableName}" GROUP BY 1, 2;""",
 
-    """SELECT "passenger_count", STRFTIME('%Y', "tpep_pickup_datetime"), ROUND("trip_distance"), COUNT(*)
-    FROM "BebraDB" GROUP BY 1, 2, 3 ORDER BY 2, 4 DESC;""",
+    f"""SELECT "passenger_count", STRFTIME('%Y', "tpep_pickup_datetime"), ROUND("trip_distance"), COUNT(*)
+    FROM "{tableName}" GROUP BY 1, 2, 3 ORDER BY 2, 4 DESC;""",
 
 ]
 
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import TryDuckDB
 
 funcs.create_db_from_csv()
 
-print("SQLite:", TrySQLite.try_sqlite3())
-print("DuckDB:", TryDuckDB.try_duckdb())
-print("Postgres:", TryPostgres.try_psycopg2())
-print("SQLAlchemy:", TrySQLAlchemy.try_sqlalchemy())
-print("Pandas:", TryPandas.try_pandas())
+if t_SQLite: print("SQLite:", TrySQLite.try_sqlite3())
+if t_DuckDB: print("DuckDB:", TryDuckDB.try_duckdb())
+if t_psycopg2: print("Postgres:", TryPostgres.try_psycopg2())
+if t_SQLAlchemy: print("SQLAlchemy:", TrySQLAlchemy.try_sqlalchemy())
+if t_Pandas: print("Pandas:", TryPandas.try_pandas())
